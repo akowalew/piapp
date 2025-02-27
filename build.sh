a02a2e72482dbe009b7cadef37c1af59453bfb0c
akowalew <ram.techen@gmail.com>
2025-02-27 11:48:36 +0000

    Build with GCC

--- build.sh
+++ build.sh
@@ -1,9 +1,6 @@
-CFLAGS="$CFLAGS -ffreestanding -nostdlib -march=armv8-a+nosimd -mtune=cortex-a53 -gdwarf-2 -g3 -O0 -ffunction-sections -fdata-sections"
-WFLAGS="$WFLAGS -Wall -Wextra -Wpedantic -Werror -Wfatal-errors -Wundef"
-XFLAGS="$XFLAGS -Wno-unused-function -Wno-unused-variable -Wno-unused-parameter -Wno-unused-but-set-variable -Wno-language-extension-token"
-LFLAGS="$LFLAGS --gc-sections"
+CFLAGS="$CFLAGS -ffreestanding -nostdlib -march=armv8-a+nosimd -mtune=cortex-a53 -gdwarf-2 -g3 -O0"
 
 mkdir -p out; cd out
-aarch64-linux-gnu-gcc-14 -c ../main.c ../start.S $CFLAGS $WFLAGS $XFLAGS
-ld.lld -m aarch64elf -nostdlib start.o main.o -T ../link.ld -o kernel8.elf $LFLAGS
-llvm-objcopy -O binary kernel8.elf ../tftp/30aa58ed/kernel8.img
+aarch64-linux-gnu-gcc -c ../main.c ../start.S $CFLAGS
+aarch64-linux-gnu-ld -m aarch64elf -nostdlib start.o main.o -T ../link.ld -o kernel8.elf
+aarch64-linux-gnu-objcopy -O binary kernel8.elf ../tftp/30aa58ed/kernel8.img
